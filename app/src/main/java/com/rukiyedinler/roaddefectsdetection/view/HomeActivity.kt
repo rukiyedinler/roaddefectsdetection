9acc61e109f291c7819b6e9f6bb73cc494c5f0af
iclalucar <iclalucar42>
2024-11-19 14:59:09 +0300

    Add ImageView and Button for photo capture to HomeActivity layout.

--- app/src/main/java/com/rukiyedinler/roaddefectsdetection/view/HomeActivity.kt
+++ app/src/main/java/com/rukiyedinler/roaddefectsdetection/view/HomeActivity.kt
@@ -1,21 +1,41 @@
 package com.rukiyedinler.roaddefectsdetection.view
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.FileProvider
 import com.rukiyedinler.roaddefectsdetection.R
+import java.io.File
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var captureIV : ImageView
+    private lateinit var imageUrl : Uri
+
+    private val contract = registerForActivityResult(ActivityResultContracts.TakePicture()){
+
+        captureIV.setImageURI(null)
+        captureIV.setImageURI(imageUrl)
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        imageUrl = createImageUri()
+        captureIV = findViewById(R.id.captureImageView)
+        val captureImgBtn = findViewById<Button>(R.id.captureImgBtn)
+        captureImgBtn.setOnClickListener {
+            contract.launch(imageUrl)
         }
+
+    }
+    private fun createImageUri():Uri {
+        val image = File(filesDir,"camera_photos.png")
+        return FileProvider.getUriForFile(this,
+            "com.rukiyedinler.roaddefectsdetection.FileProvider",
+            image)
     }
 }
