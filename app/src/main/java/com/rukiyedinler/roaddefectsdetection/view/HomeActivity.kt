52a6251e1d4afa3bee13aa83c16178c0c42f240a
iclalucar <iclalucar42>
2024-11-21 00:02:57 +0300

    Add runtime camera permission request to HomeActivity

--- app/src/main/java/com/rukiyedinler/roaddefectsdetection/view/HomeActivity.kt
+++ app/src/main/java/com/rukiyedinler/roaddefectsdetection/view/HomeActivity.kt
@@ -1,4 +1,7 @@
 package com.rukiyedinler.roaddefectsdetection.view
+import android.Manifest
+import android.content.pm.PackageManager
+
 
 import android.net.Uri
 import android.os.Bundle
@@ -9,29 +12,51 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.rukiyedinler.roaddefectsdetection.R
 import java.io.File
+import androidx.core.content.ContextCompat
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var captureIV : ImageView
     private lateinit var imageUrl : Uri
 
-    private val contract = registerForActivityResult(ActivityResultContracts.TakePicture()){
-
+    private val contract = registerForActivityResult(ActivityResultContracts.TakePicture()) {
         captureIV.setImageURI(null)
         captureIV.setImageURI(imageUrl)
-
     }
+
+    // İzin isteği sonucunu işlemek için
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // İzin verildiğinde kamera açılır
+                contract.launch(imageUrl)
+            } else {
+                // İzin verilmediğinde kullanıcıya bilgi verilebilir
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        imageUrl = createImageUri()
-        captureIV = findViewById(R.id.captureImageView)
-        val captureImgBtn = findViewById<Button>(R.id.captureImgBtn)
-        captureImgBtn.setOnClickListener {
-            contract.launch(imageUrl)
+        // İzin kontrolü
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Kamera izni verilmişse, işlemlere devam et
+            imageUrl = createImageUri()
+            captureIV = findViewById(R.id.captureImageView)
+            val captureImgBtn = findViewById<Button>(R.id.captureImgBtn)
+            captureImgBtn.setOnClickListener {
+                contract.launch(imageUrl)
+            }
+        } else {
+            // Kamera izni verilmemişse izin iste
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
-
     }
+
     private fun createImageUri():Uri {
         val image = File(filesDir,"camera_photos.png")
         return FileProvider.getUriForFile(this,
